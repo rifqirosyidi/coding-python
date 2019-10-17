e9b256cdd9a37ebf9722b1a052d400e7cea41474
Erik Dubois <erik.dubois@gmail.com>
2019-10-17 08:53:43 +0700

    string part

--- 03 String/solve.py
+++ 03 String/solve.py
@@ -1,51 +1,53 @@
-# message = input("Masukkan Kata Yang Ingin di Enkripsi : ")
-# pergeseran = int(input("Berapa karakter yang ingin di geser (1 - 26) : "))
-#
-# secret_message = ""
-#
-# for char in message:
-#     if char.isalpha():
-#         char_code = ord(char)
-#         char_code += pergeseran
-#
-#         if char.isupper():
-#             if char_code > ord('Z'):
-#                 char_code -= 26
-#             if char_code < ord('A'):
-#                 char_code += 26
-#         else:
-#             if char_code > ord('z'):
-#                 char_code -= 26
-#             if char_code < ord('a'):
-#                 char_code += 26
-#
-#         secret_message += chr(char_code)
-#
-#     else:
-#         secret_message += char
-#
-# orig_message = ""
-#
-# for char in secret_message:
-#     if char.isalpha():
-#         char_code = ord(char)
-#         char_code += pergeseran
-#
-#         if char.isupper():
-#             if char_code > ord('Z'):
-#                 char_code -= 26
-#             if char_code < ord('A'):
-#                 char_code += 26
-#         else:
-#             if char_code > ord('z'):
-#                 char_code -= 26
-#             if char_code < ord('a'):
-#                 char_code += 26
-#
-#         orig_message += chr(char_code)
-#
-#     else:
-#         orig_message += char
-#
-# print("Encrypted : ", secret_message)
-# print("Decrypted : ", orig_message)
+message = input("Masukkan Kata Yang Ingin di Enkripsi : ")
+pergeseran = int(input("Berapa karakter yang ingin di geser (1 - 26) : "))
+
+secret_message = ""
+
+for char in message:
+    if char.isalpha():
+        char_code = ord(char)
+        char_code += pergeseran
+
+        if char.isupper():
+            if char_code > ord('Z'):
+                char_code -= 26
+            if char_code < ord('A'):
+                char_code += 26
+        else:
+            if char_code > ord('z'):
+                char_code -= 26
+            if char_code < ord('a'):
+                char_code += 26
+
+        secret_message += chr(char_code)
+
+    else:
+        secret_message += char
+
+print("Encrypted : ", secret_message)
+
+orig_message = ""
+pergeseran = -pergeseran
+
+for char in secret_message:
+    if char.isalpha():
+        char_code = ord(char)
+        char_code += pergeseran
+
+        if char.isupper():
+            if char_code > ord('Z'):
+                char_code -= 26
+            if char_code < ord('A'):
+                char_code += 26
+        else:
+            if char_code > ord('z'):
+                char_code -= 26
+            if char_code < ord('a'):
+                char_code += 26
+
+        orig_message += chr(char_code)
+
+    else:
+        orig_message += char
+
+print("Decrypted : ", orig_message)
